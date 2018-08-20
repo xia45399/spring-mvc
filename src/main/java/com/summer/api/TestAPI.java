459fffda4dc523199847bde24f49e7ab29a62acf
xia45399 <453993887@qq.com>
2018-08-20 16:48:03 +0800

    配置http数据转换，解决接口直接返回中文数据时乱码

--- src/main/java/com/summer/api/TestAPI.java
+++ src/main/java/com/summer/api/TestAPI.java
@@ -3,11 +3,26 @@ package com.summer.api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/test")
 public class TestAPI {
     @RequestMapping("test1")
-    public Object test1() {
-        return "test1";
+    public Object test1(Long a) {
+        return a;
+    }
+
+    @RequestMapping("test2")
+    public Object test2(String a) {
+        return "String类型=" + a;
+    }
+
+    @RequestMapping("test3")
+    public Object test3() {
+        Map<String, String> map = new HashMap<>();
+        map.put("a", "类型");
+        return map;
     }
 }
